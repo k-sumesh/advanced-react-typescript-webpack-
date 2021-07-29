c2c91e6c0ed629da093d8477d21568bf0a7ef8c0
sumeshk789 <sumesh.k789@gmail.com>
2021-07-29 13:23:13 +0530

    react refresh plugin integrated

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,14 +1,18 @@
 const webpack = require('webpack')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 module.exports = {
   mode: 'development',
   devtool: 'cheap-module-source-map', //recomended by cra for source map
   // define env variable using webpack define plugin
   devServer: {
-    port: 3000
+    port: 3000,
+    hot: true
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.port': JSON.stringify('3000')
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin({})
   ]
 }
