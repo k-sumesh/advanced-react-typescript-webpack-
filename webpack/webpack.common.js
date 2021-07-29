9ad0d6758f4bc5067ff5364af99da963e75c688e
sumeshk789 <sumesh.k789@gmail.com>
2021-07-29 14:33:40 +0530

    bundle analyzer plugin integrated

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,14 +1,15 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, '..', 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
 
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js'],
   },
 
   module: {
@@ -16,30 +17,35 @@ module.exports = {
       {
         test: /\.css$/,
         exclude: /node_module/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(?:ico|gif|jpg|png|jpeg)$/i,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
-        type: 'asset/inline'
+        type: 'asset/inline',
       },
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader'
-          }
-        ]
-      }
-    ]
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ]
+      template: './public/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './assets', to: path.resolve(__dirname, '..', 'dist') },
+      ],
+    }),
+  ],
 }
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,15 +1,18 @@
 const webpack = require('webpack')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin // analyze bundle size
 
 module.exports = {
   mode: 'production',
   devtool: 'source-map', // recomended by cra for production,
   // define env variables so that we can set and use in tsx file main usage for having differant end points
   devServer: {
-    port: 8000
+    port: 8000,
   },
   plugins: [
     new webpack.DefinePlugin({
-      'processe.env.port': JSON.stringify('8080')
-    })
-  ]
+      'processe.env.port': JSON.stringify('8080'),
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
 }
