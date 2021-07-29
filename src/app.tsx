51ff0a36ec79e412f54a8aebfa79ba23f52a67ef
sumeshk789 <sumesh.k789@gmail.com>
2021-07-29 14:19:53 +0530

    test-husky

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,20 @@ import Image from '../assets/react.png'
 import Logo from '../assets/react-logo.svg'
 import './styles/index.css'
 export const App = () => {
+  const num = 0
+  let name ="dddddd"
+
+  let [count, setCount] = React.useState(0)
+  const hanldeButtonClick = () => {
+    setCount((count) => count + 1)
+    console.log()
+  }
   return (
     <div>
       App
       <img src={Image} alt="react logo" width="300" height="200" />
       <img src={Logo} alt="react logo" width="300" height="200" />
+      <button onClick={hanldeButtonClick}>Count{count}</button>
     </div>
   )
 }
